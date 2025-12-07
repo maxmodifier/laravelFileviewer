a8b48b859cda116c0569b1517f007ace2bb68886
maxmodifier <98882652+maxmodifier@users.noreply.github.com>
2025-12-07 13:36:04 +0530

    Delete document-preview directory

--- document-preview/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Vijay\DocumentPreview\Facades\FilePreview;
-
-Route::get('file-preview', function () {
-    \$path = request('path');
-    return FilePreview::show($path);
-});
--- document-preview/src/Exceptions/FilePreviewException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Vijay\DocumentPreview\Exceptions;
-
-use Exception;
-
-class FilePreviewException extends Exception {}
